1aa7bc15103de19c3fcfc3cc1b4520040c8930a9
Emre <118940184+Emre033@users.noreply.github.com>
2023-06-09 14:21:37 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+//AŞAGİYA ELLEME! 
+// BU ALTYAPIYI İZİNSİZ PAYLAŞMAK YASAKTIR
+require('http').createServer(function (req, res) {res.write('');res.end();}).listen(8080);
+
+const { AoiClient, LoadCommands } = require("aoi.js");
+
+const bot = new AoiClient({
+    token: "", // " İCİNE BOT TOKENİNİ YAZ!
+    prefix: "s!",
+    intents: ["Guilds", "GuildMessages", "MessageContent"],
+    events: ["onMessage", "onInteractionCreate"],
+    database: {
+        type: "aoi.db",
+        db: require("aoi.db"),
+        tables: ["main"],
+        path: "./database/",
+        extraOptions: {
+            dbType: "KeyValue"
+        }
+    }
+});
+
+const loader = new LoadCommands(bot);
+loader.load(bot.cmd, "./komutlar/") //you can change this to any directory you want
+//Ping Komutu Elleme!
+bot.command({
+name: "ping",
+code: `
+$title[1;$userTag[$clientID] | Ping]
+$description[1;
+Bot Gecikmesi: $ping 
+Mesaj Gecikmesi: $messagePing
+Veritabanı Gecikmesi: $dbPing]
+$footer[1;$userTag[$clientID] Tüm Hakları Saklıdır]
+`
+})
+
+bot.variables ({
+  abonerol:"" // ABONE OLANLARA VERİLECEK ROL " İCİNE ABONE ROL ID YAZ!
+  aboneyetkilisi:"" // ABONE YETKİLİSİ ROLU " İCİNE ROL ID YAZ!
+  
+})
+// BU ALTYAPIYI İZİNSİZ PAYLAŞMAK YASAKTIR
