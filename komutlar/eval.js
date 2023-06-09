0ffa66757e394474ebb1b498e4e572441cca7c2e
Emre <118940184+Emre033@users.noreply.github.com>
2023-06-09 14:27:35 +0300

    Create eval.js

---
+++ komutlar/eval.js
@@ -0,0 +1,12 @@
+// BU ALTYAPIYI İZİNSİZ PAYLAŞMAK YASAKTIR
+module.exports = {
+  name:"eval",
+  code:`
+  $eval[$message]
+  $onlyForIDs[SENİN İDN;]
+
+
+
+  `}
+//KOD DENEMENİZE YARAR HER KOMUTU ÇALIŞTIRMAZ!
+// BU ALTYAPIYI İZİNSİZ PAYLAŞMAK YASAKTIR
